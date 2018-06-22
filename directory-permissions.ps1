36f33b82a556b8b4b7d1d3f03a1e4036cd7b08ca
kt <38329167+kteeling@users.noreply.github.com>
2018-06-22 16:29:38 -0600

    Create directory-permissions.ps1

---
+++ directory-permissions.ps1
@@ -0,0 +1,7 @@
+# get permissions
+# Netwrix via Spiceworks
+
+$path = "\\server\share\path" #define path to the shared folder 
+$reportpath ="C:\data\ACL.csv" #define path to export permissions report 
+#script scans for directories under shared folder and gets acl(permissions) for all of them 
+dir -Recurse $path | where { $_.PsIsContainer } | % { $path1 = $_.fullname; Get-Acl $_.Fullname | % { $_.access | Add-Member -MemberType NoteProperty '.\Application Data' -Value $path1 -passthru }} | Export-Csv $reportpath
